e1264da3e9e1a881f20722c0c5d189a7063fcd20
Ian Bradley <ianbradleydebate@gmail.com>
2017-12-07 13:31:45 -0700

    test delete whitespace

--- www/src/components/Navbar.vue
+++ www/src/components/Navbar.vue
@@ -19,8 +19,6 @@
                             </div>
                         </div>
                     </div>
-
-
                     <!-- FORMS -->
                     <!-- <login></login> -->
 
