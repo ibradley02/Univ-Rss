5b81b5f6c4abac745fccda918de72b9a1d7849ec
Dan Swantek <dswantek208@gmail.com>
2017-12-07 10:33:41 -0700

    Added divs to navbar

--- www/src/App.vue
+++ www/src/App.vue
@@ -1,22 +1,27 @@
 <template>
   <div id="app">
+    <home></home>
     <img src="./assets/logo.png">
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  import Home from './components/Home'
+  export default {
+    name: 'app',
+    components: {
+      Home
+    },
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
