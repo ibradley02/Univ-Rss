5ee851c41b9f707a36e5172c564308a239cff7d5
Ian Bradley <ianbradleydebate@gmail.com>
2017-12-08 12:35:23 -0700

    cool

--- www/src/components/Navbar.vue
+++ www/src/components/Navbar.vue
@@ -1,18 +1,16 @@
 <template>
     <div class="row text-center">
         <nav class="navbar navbar-default navbar-fixed-top myNavbar">
-            <!-- WEBSITE NAME -->
-            <div class="navbar-text col-xs-12" style="font-family: 'Slabo 27px', serif">
-                <div id="content">
-                    <button type="button" id="sidebarCollapse" class="btn btn-info navbar-btn pull-left" data-toggle="collapse" data-target="#sidebar">
-                        <img src="https://www.tellusaboutus.com/comments/images/BK-WebComment/BB_WHOPPER-v1.png" alt="burger" style="max-height:50px;width:auto">
-                    </button>
-                </div>
-
-                <div>
-                    <button type="button" class="btn btn-danger pull-right" @click="logout">Logout</button>
-                </div>
+            <div class="col-sm-2">
+                <button type="button" id="sidebarCollapse" class="btn btn-info navbar-btn" data-toggle="collapse" data-target="#sidebar">
+                    <img src="https://www.tellusaboutus.com/comments/images/BK-WebComment/BB_WHOPPER-v1.png" alt="burger" class="burger">
+                </button>
+            </div>
+            <div class="col-sm-10">
                 <h1 class="univ-rss">Univ-rss</h1>
+            </div>
+            <div class="col-sm-2 pull-right">
+                <button type="button" class="btn btn-danger" @click="logout">Logout</button>
             </div>
         </nav>
     </div>
@@ -39,6 +37,11 @@
     .myNavbar {
         background-color: rgba(0, 1, 3, 1);
         color: white;
+    }
+
+    .burger{
+        max-height: 10vh;
+        max-width: 10vw;
     }
 
     .sidebar-button {
