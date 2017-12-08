9dd1f1dda8231b3e404d1d3dd69b6dd9c6868fac
jaime ambriz <jaime.ambriz@yahoo.com>
2017-12-08 15:02:22 -0700

    adding geoLocation

--- server/custom-routes/board-routes.js
+++ server/custom-routes/board-routes.js
@@ -3,21 +3,24 @@ let request = require('request')
 
 module.exports = {
     getWeather: {
-        path: '/weather',
+        path: '/weather/:lat/:long',
         reqType: 'get',
         method(req, res, next) {
             let action = 'make request to outside api and return data requested'
-            request('http://api.openweathermap.org/data/2.5/weather?q=boise&units=imperial&&APPID=bd82255fd0a21fa1238699b9eda2ee35', function (error, response, body) {
-                console.log('error:', error)
-                console.log('statusCode:', response && response.statusCode)
-                res.send(body)
+            var coor = getCoords().then(() =>{
+                request('https://api.openweathermap.org/data/2.5/weather?lat=' + req.params.lat + '&lon=' + req.params.long + '&units=imperial&&appid=8d7d6f68ddb8370dd6ae5712e11ca530', function (error, response, body) {
+                    console.log('error:', error)
+                    console.log('statusCode:', response && response.statusCode)
+                    res.send(body)
+                })
             })
         }
+
     },
     getQuote: {
         path: '/quote',
         reqType: 'get',
-        method(req, res, next){
+        method(req, res, next) {
             let action = 'make request to outside api and return data requested'
             request('http://quotesondesign.com/api/3.0/api-3.0.json', function (error, response, body) {
                 console.log('error:', error)
