bf083c7b232aadfcb45a89987903de6849ba6110
Ian Bradley <ianbradleydebate@gmail.com>
2017-12-14 10:59:34 -0700

    added feedly to dev server

--- server/config/dev-server.js
+++ server/config/dev-server.js
@@ -7,6 +7,13 @@ var corsOptions = require('./handlers').corsOptions
 var api = require('../models')
 var session = require('../authentication/sessions')
 var Auth = require('../authentication/auth')
+var Feedly = require('feedly')
+
+var f = new Feedly({
+    client_id: 'd4600ded-885d-4173-b9ed-8f41592b1d6e',
+    client_secret: 'A3H3PsHe7hDKWyzRZJb2ycIV4FwIhzE0FbEMrBKIkNvIt8Ofoqy_u0zI95xDuzRuQd2niCZboEdwN89QH2DvSJrJmum9UoWLATkDzDdZa4VfgRjJSThRYtce_gOnGEIf6_NLEDqNogwJZuSF7OhLUYoersoJgcIF6x7CVWvHms5QSJeP5eNKsXxfzlB1HGL1Ozefm9tVrA_zzlDp7vtRtvDCBRln5lg:feedlydev',
+    port: 8080
+})
 
 // ENABLE ROUTES IF USING app SIDE ROUTING
 // import routes from './routes'
