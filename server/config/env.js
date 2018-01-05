ee1add4323759990188af9987060d0caf4b1aef5
oconnill <doconnelldesign@gmail.com>
2018-01-05 10:35:42 -0700

    new server established

--- server/config/env.js
+++ server/config/env.js
@@ -2,13 +2,13 @@ let env = {
 	NODE_ENV: 'development',
 	PORT: 3000,
 	DBPROTOCOL: 'mongodb',
-	DBUSERNAME: 'Admin',
-	DBPASSWORD: 'admin123',
-	DBHOST: 'ds044887.mlab.com:44887',
-	DBNAME: 'univ-rss',
+	DBUSERNAME: 'test',
+	DBPASSWORD: 'test',
+	DBHOST: 'ds143907.mlab.com:43907',
+	DBNAME: 'capstone',
 	SERVERNAME: 'dev-server'
 }
-
+mongodb://<dbuser>:<dbpassword>@ds143907.mlab.com:43907/capstone
 // MAPS env TO ACTUAL ENVIRONMENT
 Object.keys(env).forEach(v => {
 	process.env[v] = process.env[v] || env[v]
